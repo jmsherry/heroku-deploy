5d040a2bd4a9513477816cfde2a37ad81214287f
James Sherry <james.m.sherry@googlemail.com>
2021-10-27 21:03:58 +0100

    initial files

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+
+const { MESSAGE = "DEFAULT MESSAGE", PORT = 5000 } = process.env;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get(`/api`, (req, res) => {
+  console.log("MESSAGE", MESSAGE);
+  res.status(200).send(MESSAGE);
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
